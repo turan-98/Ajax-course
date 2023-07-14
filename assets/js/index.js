5aaf285d40bc4384bcddf2dd20b052c0e7d358d5
turan-98 <santanasamuel08@gmail.com>
2023-07-14 16:43:24 -0300

    Changing the style, improving code quality javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ document.addEventListener('click', e => {
         e.preventDefault();
         loadPage(el);
         var $activeTab = document.activeElement;
-        console.log('teste ' + $activeTab)
+        changeClass($activeTab);
     }
 })
 async function loadPage(elem) {
@@ -25,3 +25,29 @@ function loadResult(response) {
     const result = document.querySelector('.result');
     result.innerHTML = response;
 }
+
+function changeClass(el){
+    var tabOne = document.querySelector("#linkOne");
+    var tabTwo = document.querySelector("#linkTwo");
+    var tabThree = document.querySelector("#linkThree");
+    var tabs = el;
+
+    tabs = tabs.toString();
+    
+    if(tabs.includes('indexPage1.html')){
+        tabTwo.classList.add('active');
+        tabOne.classList.remove('active');
+        tabThree.classList.remove('active')
+    }
+    else if(tabs.includes('indexPage2.html')){
+        tabThree.classList.add('active')
+        tabTwo.classList.remove('active');
+        tabOne.classList.remove('active');
+    }
+    else{
+        tabOne.classList.add('active');
+        tabThree.classList.remove('active')
+        tabTwo.classList.remove('active');
+    }
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports ={
-    mode:"development",
+    mode:"production",
     entry: path.join(__dirname, "assets","js","index.js"),
     output:{
         path: path.resolve(__dirname, 'dist'),
